75a20626df28f4bd2f1dde8d2bba93925e68f1a1
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-11-28 23:29:05 +0545

    add: make the screen dark and light mode

--- js/style-switcher.js
+++ js/style-switcher.js
@@ -27,3 +27,15 @@ function setActiveStyle(color) {
     }
   })
 }
+/*============== theme light and dark mode ============*/ 
+const dayNight = document.querySelector(".day-night");
+window.addEventListener("load", ()=> {
+  if(document.body.classList.contains("dark"))
+  {
+    dayNight.querySelector("i").classList.add("fa-sun");
+  }
+  else 
+  {
+    dayNight.querySelector("i").classList.add("fa-moon");document.body.classList.contains("dark")
+  }
+})
