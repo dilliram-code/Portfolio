f552ab0473b49671deb290137efbe5d0539c2087
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-12-01 20:47:18 +0545

    link external websites with the images in the websites in the portfolio section.

--- js/script.js
+++ js/script.js
@@ -4,3 +4,23 @@ var typed = new Typed(".typing", {
   BackSpeed: 60,
   loop:true
 })
+
+//////////////////////////////////////////////////////////////
+// Add links to the portfolio images for the external website.
+// Link to the first image
+let firstLink = document.querySelector('.portfolio-image1');
+firstLink.addEventListener('click', () => {
+  window.location.href = 'https://enjoyaroom.netlify.app/';
+});
+
+//Link to the second image
+let secondLink = document.querySelector('.portfolio-image2');
+secondLink.addEventListener('click', ()=>{
+  window.location.href='https://lowbudgetgroup.netlify.app/';
+});
+
+//Link to the third image
+let thirdLink = document.querySelector('.portfolio-image3');
+thirdLink.addEventListener('click', ()=> {
+  window.location.href = 'https://dilliram.vercel.app/';
+})
