c85dd0cc11d27bf0d79938ff98d4ec43eed4b451
Dilli Ram Chaudhary <chaudharydr23@gmail.com>
2024-12-02 21:42:16 +0545

    feat: link emailjs service and  add the feature to send the email to my email directly

--- js/script.js
+++ js/script.js
@@ -24,3 +24,40 @@ let thirdLink = document.querySelector('.portfolio-image3');
 thirdLink.addEventListener('click', ()=> {
   window.location.href = 'https://dilliram.vercel.app/';
 })
+
+
+// Email Js starts
+
+// Initialize EmailJS with your user ID
+emailjs.init('hFrNm3XDp2pxj6OtW'); 
+  // Wait for the page to load
+  document.addEventListener('DOMContentLoaded', function () {
+    // Add an event listener to the submit button
+    document.querySelector('.btn').addEventListener('click', function (e) {
+      e.preventDefault(); 
+
+      // Get the form data
+      const name = document.querySelector('input[placeholder="Name"]').value;
+      const email = document.querySelector('input[placeholder="Email"]').value;
+      const subject = document.querySelector('input[placeholder="Subject"]').value;
+      const message = document.querySelector('textarea[placeholder="Message"]').value;
+
+      // Use EmailJS to send the data
+      emailjs.send('service_f7fz87m', 'template_965xjpo', {
+        name: name,
+        email: email,
+        subject: subject,
+        message: message
+      }).then(function(response) {
+        console.log('Success!', response.status, response.text);
+        alert('Message sent successfully!');
+      }, function(error) {
+        console.log('Failed...', error);
+        alert('Failed to send message. Please try again later.');
+      });
+    });
+  });
+
+
+
+// Eamil Js ends
